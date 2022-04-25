541f2fefd7c305d19e7fe26ed274b9ecfd1be192
vishal <vishal@LAPTOP-EHMVTDSD>
2022-04-25 12:19:36 +0530

    By vish

--- vishalerror/src/vishalerror/xyz.java
+++ vishalerror/src/vishalerror/xyz.java
@@ -2,9 +2,17 @@ package vishalerror;
 
 public class xyz 
 {
+	public void m1()
+	{
+		System.out.println("M1 method calling");
+	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 		System.out.println("vishal dada");
+		
+		xyz a=new xyz();
+		a.m1();
 	}
 }
