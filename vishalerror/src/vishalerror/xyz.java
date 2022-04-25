26b31696f0c5ef60b7a55cf778670273165e73e1
Vishal <104187547+vvdalvi@users.noreply.github.com>
2022-04-25 12:30:33 +0530

    Update xyz.java

--- vishalerror/src/vishalerror/xyz.java
+++ vishalerror/src/vishalerror/xyz.java
@@ -6,6 +6,11 @@ public class xyz
 	{
 		System.out.println("M1 method calling");
 	}
+	
+	public void m2()
+	{
+		System.out.println("M2 method calling");
+	}
 
 	public static void main(String[] args) 
 	{
